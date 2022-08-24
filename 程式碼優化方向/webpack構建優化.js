c7f43556a38c8927e6b2e5075866c73dc52267bc
洪承泓 <jasonhung@hongchenghongdeMacBook-Pro.local>
2022-08-24 11:28:26 +0800

    [前端性能優化] - webpack持續補充

--- 程式碼優化方向/webpack構建優化.js
+++ 程式碼優化方向/webpack構建優化.js
@@ -15,6 +15,35 @@ ex:
 // 程式碼體積減小
 // 提高執行效率
 // Babel的module設置要為false
+```
+/****************** util.js ******************/
+export default 'Hello,Webpack';
+
+/**************** index.jsx ********************/
+import str from './util';
+console.log(str);
+
+/***************** 没有 scope hoisting, webpack 打包後 *******************/
+[
+  (function (module, __webpack_exports__, __webpack_require__) {
+    var __WEBPACK_IMPORTED_MODULE_0__util_js__ = __webpack_require__(1);
+    console.log(__WEBPACK_IMPORTED_MODULE_0__util_js__["a"]);
+  }),
+  (function (module, __webpack_exports__, __webpack_require__) {
+    __webpack_exports__["a"] = ('Hello,Webpack');
+  })
+]
+/************************************/
+
+/***************** 有 scope hoisting, webpack 打包後 *******************/
+[
+  (function (module, __webpack_exports__, __webpack_require__) {
+    var util = ('Hello,Webpack');
+    console.log(util);
+  })
+]
+/************************************/
+```
 
 /* babel 設定 --------------------------------------------------------------------------------------------------------- */
 // polyfill的需要加載
@@ -26,4 +55,37 @@ presets: [
 // 輔助函數的複用
 plugins: [  
   "@babel/plugin-transform-runtime",
+]
+
+/* noParse --------------------------------------------------------------------------------------------------------- */
+// 提高打包速度
+// 忽略較大的lib不要加入解析
+// 被忽略的條件為 不能有import, require, define的引入方式
+
+/* DllPlugin --------------------------------------------------------------------------------------------------------- */
+// 避免打包時對不變的lib重複構建 ex: vue, react
+
+/* minification --------------------------------------------------------------------------------------------------------- */
+// Terser 壓縮js
+// mini-css-extract-plugin 壓縮css
+npm i mini-css-extract-plugin optimize-css-assets-webpack-plugin
+plugins: [
+  // css 提取
+  new MiniCssExtractPlugin({
+    filename: '[name].[contenthash].css',
+    chunkFilename: '[id].[contenthash:8].css',
+  }),
+  // css 優化(下面的配置是刪除註釋)
+  new OptimizeCssAssetsPlugin({
+    cssProcessorPluginOptions: {
+        preset: ['default', {discardComments: {removeAll: true}}],
+    },
+    canPrint: true
+  }),
+]
+// HtmlWebpackPlugin(默認使用minify: html-minifier-terser) 壓縮html
+plugins: [
+  new HtmlWebpackPlugin({
+    template: 'target.html', 
+  }),
 ]
